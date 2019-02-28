fd534918b7100690418560d537133d7db1099dfb
Mariana Fidalgo <mariana.fidalgo.f@tecnico.ulisboa.pt>
2019-02-28 09:21:36 +0000

    initial commit

---
+++ tcpserver.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h> 
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void)
+{
+	struct addrinfo hints, *res;
+	int newfd, fd,addrlen,n,nread;
+	struct sockaddr_in addr;
+	char buffer[128];
+
+	memset(&hints,0,sizeof hints);
+	hints.ai_family=AF_INET; //IPv4
+	hints.ai_socktype=SOCK_STREAM; //TCP socket
+	hints.ai_flags= AI_PASSIVE|AI_NUMERICSERV;
+
+	n= getaddrinfo(NULL,"58001",&hints,&res);
+	if(n!=0)/*error*/ 
+		exit(1);
+
+	fd=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+	if(fd==-1)/*error*/
+		exit(1);
+
+	n=bind(fd,res->ai_addr,res->ai_addrlen);
+	if(n==-1)/*error*/
+		exit(1);
+
+	if(listen(fd,5)==-1)/*error*/
+		exit(1);
+
+	if((newfd=accept(fd,(struct sockaddr*)&addr,&addrlen))==-1)/*error*/
+		exit(1);
+
+	n=read(newfd,buffer,128);
+	if(n==-1)/*error*/
+		exit(1);
+
+	write(1, "received: ",10);
+	write(1, buffer, n);
+
+	n= write(newfd,buffer,n);
+	if(n==-1)/*error*/
+		exit(1);
+
+	freeaddrinfo(res);
+	close(fd);
+	exit(0);
+}
---
+++ udpclient.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h> 
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT "59000"
+
+int main(void)
+{
+	struct addrinfo hints, *res;
+	int fd,addrlen,n,nread;
+	struct sockaddr_in addr;
+	char buffer[128];
+
+	memset(&hints,0,sizeof hints);
+	hints.ai_family=AF_INET; //IPv4
+	hints.ai_socktype=SOCK_DGRAM; //UDP socket
+	hints.ai_flags= AI_NUMERICSERV;
+
+	n= getaddrinfo("tejo.tecnico.ulisboa.pt",PORT,&hints,&res);
+	if(n!=0)/*error*/ 
+		exit(1);
+
+	fd=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+	if(fd==-1)/*error*/
+		exit(1);
+
+	strcpy(buffer, "WHOISROOT abc:1.1.1.1:1 2.2.2.3:2\n"); 
+	n= sendto(fd,buffer,strlen(buffer),0,res->ai_addr,res->ai_addrlen);
+	if(n==-1)/*error*/
+		exit(1);
+
+	addrlen=sizeof(addr);
+	nread=recvfrom(fd,buffer,128,0,(struct sockaddr*)&addr,&addrlen);
+	if(nread ==-1)/*error*/
+		exit(1);
+	write(1,"echo: ",6);
+	write(1,buffer,n);
+
+
+	//freeaddrinfo(res); 
+	close(fd);
+	exit(0);
+}
